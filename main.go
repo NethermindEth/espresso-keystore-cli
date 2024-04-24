c260eb53c52159c06144460af725b6204a1f3b06
AntiD2ta <mtpotro41@gmail.com>
2024-04-24 23:46:52 +0000

    feat: Update logic

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	secretmanagerpb "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
@@ -33,7 +34,7 @@ func main() {
 	}
 
 	// Existing secrets will be stored in this map
-	existingSecrets := make(map[string]string)
+	secrets := make(map[string]string)
 
 	// Load current secret contents
 	secretData, err := getSecret(ctx, client, projectID, secretID, "latest")
@@ -45,11 +46,12 @@ func main() {
 	lines := strings.Split(string(secretData), "\n")
 	for _, line := range lines {
 		if keyValue := strings.SplitN(line, "=", 2); len(keyValue) == 2 {
-			existingSecrets[keyValue[0]] = keyValue[1]
+			secrets[keyValue[0]] = keyValue[1]
 		}
 	}
 
 	// Process each .env file
+	updated := false
 	for _, file := range files {
 		if filepath.Ext(file.Name()) == ".env" {
 			contents, err := os.ReadFile(filepath.Join(keysPath, file.Name()))
@@ -61,13 +63,32 @@ func main() {
 			// Index is assumed from file name, extracting from 'x.env'
 			index := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name()))
 
-			// Process and update the secret
-			err = processAndUpdateSecret(ctx, client, projectID, secretID, string(contents), index, existingSecrets)
-			if err != nil {
-				log.Printf("Error processing file %s: %v", file.Name(), err)
+			lines := strings.Split(string(contents), "\n")
+			for _, line := range lines {
+				if keyValue := strings.SplitN(line, "=", 2); len(keyValue) == 2 {
+					key := fmt.Sprintf("%s_%s", keyValue[0], index)
+					if _, exists := secrets[key]; !exists {
+						secrets[key] = keyValue[1]
+						updated = true
+					}
+				}
 			}
 		}
 	}
+	if updated {
+		// Process and update the secret
+		err = updateSecret(ctx, client, projectID, secretID, secrets)
+		if err != nil {
+			log.Printf("Error updating secret: %v", err)
+		}
+		log.Printf("Secrets updated")
+	} else {
+		log.Printf("No new secrets to update")
+	}
+
+	// Sleep for 5 seconds to allow the secret to propagate
+	log.Printf("Sleeping for 5 seconds to allow the secret to propagate")
+	<-time.After(5 * time.Second)
 
 	// Reload and print final secret contents
 	finalSecretData, err := getSecret(ctx, client, projectID, secretID, "latest")
@@ -88,32 +109,22 @@ func getSecret(ctx context.Context, client *secretmanager.Client, projectID, sec
 	return result.Payload.Data, nil
 }
 
-func processAndUpdateSecret(ctx context.Context, client *secretmanager.Client, projectID, secretID, contents, index string, existingSecrets map[string]string) error {
-	lines := strings.Split(contents, "\n")
-	updated := false
+func updateSecret(ctx context.Context, client *secretmanager.Client, projectID, secretID string, secrets map[string]string) error {
 	var newSecretValue string
 
-	for _, line := range lines {
-		if keyValue := strings.SplitN(line, "=", 2); len(keyValue) == 2 {
-			key := fmt.Sprintf("%s_%s", keyValue[0], index)
-			if _, exists := existingSecrets[key]; !exists {
-				newSecretValue += fmt.Sprintf("%s=%s\n", key, keyValue[1])
-				updated = true
-			}
-		}
+	for key, value := range secrets {
+		newSecretValue += fmt.Sprintf("%s=%s\n", key, value)
 	}
 
-	if updated {
-		secretVersionRequest := &secretmanagerpb.AddSecretVersionRequest{
-			Parent: fmt.Sprintf("projects/%s/secrets/%s", projectID, secretID),
-			Payload: &secretmanagerpb.SecretPayload{
-				Data: []byte(newSecretValue),
-			},
-		}
-		_, err := client.AddSecretVersion(ctx, secretVersionRequest)
-		if err != nil {
-			return err
-		}
+	secretVersionRequest := &secretmanagerpb.AddSecretVersionRequest{
+		Parent: fmt.Sprintf("projects/%s/secrets/%s", projectID, secretID),
+		Payload: &secretmanagerpb.SecretPayload{
+			Data: []byte(newSecretValue),
+		},
+	}
+	_, err := client.AddSecretVersion(ctx, secretVersionRequest)
+	if err != nil {
+		return err
 	}
 	return nil
 }
